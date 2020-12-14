ca4415f9bfb135c41bf358daf18cfc81967df10f
Your Username <you@example.com>
2020-12-14 16:37:04 -0700

    creating 3 required routes in app.js activate yarn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import PositionsIndex from './components/PositionsIndex';
+import Position from './components/PositionFrom';
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="positions" component={PositionsIndex}/>
+          <Route path="/positions/new" component={PositionForm}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
