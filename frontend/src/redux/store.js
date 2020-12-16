fb925bee479ccc427070ce529e2e4d25e02f0a99
Your Username <you@example.com>
2020-12-16 16:04:07 -0700

    created store.js

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,16 @@
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
+import positionReducer from './reducers/positionReducer'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEFTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    positions: positionReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
