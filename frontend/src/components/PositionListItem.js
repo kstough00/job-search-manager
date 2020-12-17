5c41d589a6fef91722ac012739d71aec3c224fcd
Your Username <you@example.com>
2020-12-17 16:06:32 -0700

    connecting deletePosition action to listitem compon.

--- frontend/src/components/PositionListItem.js
+++ frontend/src/components/PositionListItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaTrash } from "react-icons/fa"
+import { deletePosition } from '../redux/actions/positionActions'
+import { connect } from "react-redux";
 
-export default function PositionListItem({ position }) {
+export default function PositionListItem({ position, deletePosition }) {
+    const deletePosition = () => {
+
+    }
     return (
         <div>
             <ul>
-                <h5><Link to="/position/:id"> {position.title}</Link></h5> - {position.company} - ${position.salary}
+                <h4><Link to="/position/:id"> {position.title}</Link></h4> Company: {position.company} - ${position.salary}
+                <FaTrash onClick={ deletePosition } style={{ cursor: "pointer" }} />
             </ul>
         </div>
     )
 }
+
+export default connect(null, {deletePosition})(PositionListItem)
