9c9baaca7b8eb9fd43a898d7ffd211511db06d50
Your Username <you@example.com>
2020-12-17 13:18:20 -0700

    added submit function for position form

--- frontend/src/components/PositionForm.js
+++ frontend/src/components/PositionForm.js
@@ -6,24 +6,30 @@ export default class PositionForm extends Component {
         company: "",
         salary: "",
     };
-    submit = e => {
-        e.preventDefault()
 
-    }
+    submit = (e) => {
+        e.preventDefault();
+        this.setState({
+            title: "",
+            company: "",
+            salary: "",
+        });
+    };
+    
     render() {
         return (
             <div>
-            <h1>Add applied Position</h1>
-            <form onSubmit={this.submit}>
-                Title: <input onChange={e => this.setState({title: e.target.value})} 
-                type="text" value={this.state.title}/>
-                Company: <input onChange={e => this.setState({company: e.target.value})}
-                type="text" value={this.state.company}/>
-                Salary: <input onChange={e => this.setState({salary: e.target.value})}
-                type="text" value={this.state.salary}/>
-                <input type="submit" value="create position" />
-            </form>
-        </div>
+                <h1>Add Position</h1>
+                    <form onSubmit={this.submit}>
+                        Title: <input onChange={e => this.setState({title: e.target.value})} 
+                        type="text" value={this.state.title}/>
+                        Company: <input onChange={e => this.setState({company: e.target.value})}
+                        type="text" value={this.state.company}/>
+                        Salary: <input onChange={e => this.setState({salary: e.target.value})}
+                        type="text" value={this.state.salary}/>
+                        <input type="submit" value="create position" />
+                    </form>
+            </div>
         )
     }
 }
