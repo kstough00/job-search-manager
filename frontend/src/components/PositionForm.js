8765c01a403e507097d640f976d6c6af286dbb75
Your Username <you@example.com>
2020-12-16 17:11:51 -0700

    position form created

--- frontend/src/components/PositionForm.js
+++ frontend/src/components/PositionForm.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-export default function PositionForm() {
-    return (
-        <div>
+export default class PositionForm extends Component {
+    state = {
+        title: "",
+        company: "",
+        salary: "",
+    };
+    submit = e => {
+        e.preventDefault()
+
+    }
+    render() {
+        return (
+            <div>
             <h1>Add applied Position</h1>
+            <form onSubmit={this.submit}>
+                Title: <input onChange={e => this.setState({title: e.target.value})} 
+                type="text" value={this.state.title}/>
+                Company: <input onChange={e => this.setState({company: e.target.value})}
+                type="text" value={this.state.company}/>
+                Salary: <input onChange={e => this.setState({salary: e.target.value})}
+                type="text" value={this.state.salary}/>
+                <input type="submit" value="create position" />
+            </form>
         </div>
-    )
+        )
+    }
 }
