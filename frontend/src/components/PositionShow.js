9d660706428cb1f824d8833812b77c6e463f6f81
Your Username <you@example.com>
2020-12-18 10:50:41 -0700

    created positionshow component

---
+++ frontend/src/components/PositionShow.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { deletePosition } from "../redux/actions/positionActions";
+import { connect } from "react-redux";
+
+export default function PositionShow() {
+  return <div> Hello World! </div>;
+}
