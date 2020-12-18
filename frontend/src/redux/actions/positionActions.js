e32ba2a2518f8657361642e67596c8871e4783e0
Your Username <you@example.com>
2020-12-18 10:50:00 -0700

    added getposition action

--- frontend/src/redux/actions/positionActions.js
+++ frontend/src/redux/actions/positionActions.js
@@ -1,35 +1,45 @@
 export const getPositions = () => {
-    return (dispatch) => {
-        fetch(`http://localhost:3001/positions`)
-        .then((res) => res.json())
-        .then((positions) => 
-            dispatch({ type: "FETCH_POSITIONS_SUCCESS", payload: positions })
-        );
-    };
+  return (dispatch) => {
+    fetch(`http://localhost:3001/positions`)
+      .then((res) => res.json())
+      .then((positions) =>
+        dispatch({ type: "FETCH_POSITIONS_SUCCESS", payload: positions })
+      );
+  };
 };
 
-export const createPosition = data => {
-    return (dispatch) => {
-        fetch(`http://localhost:3001/positions`, {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ position: data }),
-        })
-        .then((res) => res.json())
-        .then((position) => 
+export const createPosition = (data) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3001/positions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ position: data }),
+    })
+      .then((res) => res.json())
+      .then((position) =>
         dispatch({ type: "CREATE_POSITION_SUCCESS", payload: position })
-        );
-    }
-}
+      );
+  };
+};
 
 export const deletePosition = (positionId) => {
-    return (dispatch) => {
-        fetch(`http://localhost:3001/positions/${positionId}`, {
-            method: "DELETE"
-        })
-            .then((res) => res.json())
-            .then((id) => console.log(id));
-    };
+  return (dispatch) => {
+    fetch(`http://localhost:3001/positions/${positionId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(({ id }) => dispatch({ type: "DELETE_POSITION", payload: id }));
+  };
+};
+
+export const getPosition = (positionId) => {
+  return (dispatch) => {
+    fetch(`http://localhost:3001/positions/${positionId}`)
+      .then((res) => res.json())
+      .then((position) =>
+        dispatch({ type: "FETCH_POSITION_SUCCESS", payload: position })
+      );
+  };
 };
