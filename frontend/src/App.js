6da50fef56fe790ef2cfe8fcf6d03e0b836374bb
Your Username <you@example.com>
2020-12-18 10:56:56 -0700

    added positionshow route and imported component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import PositionIndex from "./components/PositionIndex";
 import PositionForm from "./components/PositionForm";
+import PositionShow from "./components/PositionShow";
 import Nav from "./components/Nav";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/positions" component={PositionIndex} />
           <Route path="/positions/new" component={PositionForm} />
+          <Route exact path="/positions/:id" component={PositionShow} />
         </Switch>
       </Router>
     </div>
