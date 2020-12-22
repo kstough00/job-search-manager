5ff71f5506806ffad9a99f2472e12b4026e32d1f
Your Username <you@example.com>
2020-12-22 12:48:37 -0600

    added link to position show route

--- frontend/src/components/PositionListItem.js
+++ frontend/src/components/PositionListItem.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaTrash } from "react-icons/fa"
-import { deletePosition } from '../redux/actions/positionActions'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaTrash } from "react-icons/fa";
+import { deletePosition } from "../redux/actions/positionActions";
 import { connect } from "react-redux";
 
 function PositionListItem({ position, deletePosition }) {
-    const submitDelete = () => {
-        deletePosition(position.id);
-    };
-    return (
-        <div>
-            <ul>
-                <h4><Link to="/position/:id"> {position.title}</Link></h4> Company: {position.company} - ${position.salary}
-                <FaTrash onClick={submitDelete} style={{ cursor: "pointer" }} />
-            </ul>
-        </div>
-    );
+  const submitDelete = () => {
+    deletePosition(position.id);
+  };
+  return (
+    <div>
+      <ul>
+        <h4>
+          <Link to={`/positions/${position.id}`}> {position.title}</Link>
+        </h4>{" "}
+        Company: {position.company} - ${position.salary}
+        <FaTrash onClick={submitDelete} style={{ cursor: "pointer" }} />
+      </ul>
+    </div>
+  );
 }
 
-export default connect(null, {deletePosition})(PositionListItem)
+export default connect(null, { deletePosition })(PositionListItem);
