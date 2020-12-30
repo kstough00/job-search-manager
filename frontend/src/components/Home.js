5930c5e6d728ee9c72102fcb1500bc9fcba0b1b7
Your Username <you@example.com>
2020-12-30 10:54:33 -0700

    added some images and text to home/nav

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import PositionListItem from './PositionListItem'
+import React from "react"; //imports react library from react
+import PositionListItem from "./PositionListItem";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            New Positions: {PositionListItem.call.length}
-        </div>
-    )
+  return (
+    <div
+      style={{
+        backgroundImage: "url(" + "" + ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1>Welcome To Job Search Manager!</h1>
+      <h4>
+        Tired of organizing miscellaneous notes or maticulously updating your
+        Excel spreadsheet when searching for a job? Job Search Manager is the
+        only application you will need to make your job search simple,
+        organized, and stress free. Manage your applied positions details and
+        keep track of your application progress all in one place!
+      </h4>
+      New Positions this Week: {PositionListItem.call.length}
+      <br></br>
+      <p></p>
+      <img src="https://images.techhive.com/images/article/2016/11/technology_resume-100690687-large.jpg"></img>
+      <p>
+        About- Special 'Thanks' to COVID-19 for making this website possible; I
+        was inspired by my current 'Unemployment' status and upcoming Job Search
+        curriculum track so that I will finally be able to re-enter the
+        workforce and regain some sense of normalcy. I am beyond excited!
+      </p>
+    </div>
+  );
 }
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,9 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from "react"; //imports react library from react
+import { Link } from "react-router-dom"; //import link to make use of link functionality on our webpage
 
 export default function Nav() {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          "https://thumbs.dreamstime.com/z/job-opportunity-written-brown-paper-white-paper-teardrop-44969284.jpg" +
+          ")",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
       <ul style={{ textAlign: "left" }}>
         <Link to="/">Home </Link>
         <br></br>
